e52fbdb3cc5ef0dfae98b5994dc6cdd15849c3e6
LukaDimaa <ldimanoski20@gmail.com>
2026-03-28 21:34:41 +1100

    Testing if the program runs which it does and it allows us to basically provide the CD once we give it via /home/user/Documents then showing all of the FILES,DIR...etc

---
+++ FileHunter.py
@@ -0,0 +1,22 @@
+import os 
+#Import the OS Files Folders Paths etc.
+
+print("=== FileHunter V1 ===")
+
+path =  input("Enter a folder path to scan")
+
+if os.path.exists(path) and os.path.isdir(path): #Checking if the path exists 
+    print(F"\nScanning folder: {path}\n")
+
+    for item in os.listdir(path):
+        full_path = os.path.join(path, item)
+
+        if os.path.isfile(full_path):
+            print(f"[FILE] {item}")
+        elif os.path.isdir(full_path):
+            print(F"[DIR ] {item}")
+
+else:
+    print("Invalid Folder Path.")
+
+#Test This with different Folder and Files paths
